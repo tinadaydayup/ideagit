b4fe9e86c0f144fdeba9fd8c2b4760605fb6ba9d
Tina <1289813875@qq.com>
2020-05-15 14:21:23 +0800

    tianjia分支2

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -3,6 +3,7 @@ package com.atguigu.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("gittestidea");
-        System.out.println("branch");
+        System.out.println("branch1");
+        System.out.println("branch2");
     }
 }
